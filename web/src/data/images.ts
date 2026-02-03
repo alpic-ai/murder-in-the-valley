7d580e4a793d6a78fecd7030b800c5eb0c57cb1a
Charles Sonigo <charles.sonigo@gmail.com>
2026-02-03 11:31:59 +0100

    Refactor murder widget structure

---
+++ web/src/data/images.ts
@@ -0,0 +1,12 @@
+import samImage from "../../assets/sam.png";
+import darioImage from "../../assets/dario.png";
+import elonImage from "../../assets/elon.png";
+import alpicLogo from "../../assets/alpic.svg";
+
+export { samImage, darioImage, elonImage, alpicLogo };
+
+export const suspectImages: Record<string, string> = {
+  Sam: samImage,
+  Dario: darioImage,
+  Elon: elonImage,
+};
